fc7ba3b795473c10dbd2bf169fd2eaeff00e99f5
kyomin <kim031504@naver.com>
2020-10-01 03:28:48 +0900

    백준 2744 - 대소문자 바꾸기 : 문자열 처리

--- 2744.cpp
+++ 2744.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	string str;
 	cin >> str;
 
 	int len = str.size();
 
 	for (int i = 0; i < len; i++) {
-		if (isupper(str[i])) {
+		if (isupper(str[i]))
 			str[i] = (char)(str[i] + 32);
-		}
-		else {
-			str[i] = (char)(str[i] - 32);
-		}			
+		else
+			str[i] = (char)(str[i] - 32);		
 	}
 
 	cout << str << '\n';
