622a3aa46a3596f21b20f52777ef6606c5a2352d
kyomin <kim031504@naver.com>
2020-10-01 01:41:05 +0900

    백준 1592 - 영식이와 친구들 : 수학, 시뮬레이션

--- 1592.cpp
+++ 1592.cpp
@@ -1,52 +1,43 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-// first : 사람 번호, second : 공 받은 횟수 
-vector<pair<int, int>> friends;
+int friends[1001];
 int N, M, L;
-int cnt = 1;	// 횟수
-int cur = 1;	// 현재 공 가지고 있는 사람
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	cin >> N >> M >> L;
 
-	// 인덱스는 1부터 시작하기 위함이다.
-	friends.resize(N + 1);
-
-	// 처음 1번은 공을 받은 상태이다.
-	friends[1] = make_pair(1, 1);
-
-	// 나머지 친구들 입력
-	for (int i = 2; i <= N; i++) {
-		friends[i] = make_pair(i, 0);
-	}
+	// 1번 자리에 앉은 친구가 처음 공을 받는 경우를 처리
+	int ans = 0;		
+	int cur_friend = 1;
+	friends[cur_friend]++;
 
 	while (true) {
-		// M번 잡은 사람이 생기면 게임 종료!
-		if (friends[cur].second == M)
-			break;
-		
-		// 현재 잡은 횟수가 홀수이면 시계방향으로 작업
-		if (friends[cur].second % 2 == 1) {
-			cur = (friends[cur].first + L) % N;
-			friends[cur].second += 1;
-			cnt++;
-		} else {	// 반시계 방향 작업
-			if (cur - L <= 0) {
-				cur = (cur - L) + N;
-				friends[cur].second += 1;
-				cnt++;
-			} else {
-				cur = (cur - L);
-				friends[cur].second += 1;
-				cnt++;
-			}
-		}
+		int cur_cnt = friends[cur_friend];
+		if (cur_cnt == M) break;
+
+		/*
+			현재 받은 사람이 홀수번 받았을 경우 -> 시계 방향(+L)
+			현재 받은 사람이 짝수번 받았을 경우 -> 반시계 방향(-L)
+		*/
+		cur_friend = (cur_cnt % 2 == 1) ? (cur_friend + L) : cur_friend - L;
+
+		// 다음 받을 사람이 배열의 범위를 벗어났을 경우(시계 방향일 때)
+		if (cur_friend > N)
+			cur_friend %= N;
+		// 다음 받을 사람이 배열의 범위를 벗어났을 경우(반시계 방향일 때)
+		else if (cur_friend < 1)
+			cur_friend += N;
+
+		friends[cur_friend]++;	// 공 받는 사람 ++
+		ans++;	// 공 던진 횟수 ++
 	}
 
-	cout << cnt << '\n';
+	cout << ans << '\n';
 
 	return 0;
 }
