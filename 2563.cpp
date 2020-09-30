ca4f46bc2524b9a03c928a2512c195d6abd38cf2
kyomin <kim031504@naver.com>
2020-10-01 03:10:38 +0900

    백준 2563 - 색종이 : 구현, 시뮬레이션

--- 2563.cpp
+++ 2563.cpp
@@ -5,30 +5,28 @@ using namespace std;
 bool check[1000][1000];
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int N;
 	cin >> N;
 
 	for (int n = 0; n < N; n++) {
 		int W, H;
-
 		cin >> W >> H;
 
-		for (int h = H; h < H + 10; h++) {
-			for (int w = W; w < W + 10; w++) {
+		for (int h = H; h < H + 10; h++)
+			for (int w = W; w < W + 10; w++)
 				if (!check[h][w])
 					check[h][w] = true;
-			}
-		}
 	}
 
 	int ans = 0;
 
-	for (int i = 0; i < 1000; i++) {
-		for (int j = 0; j < 1000; j++) {
+	for (int i = 0; i < 1000; i++)
+		for (int j = 0; j < 1000; j++)
 			if (check[i][j])
 				ans++;
-		}
-	}
 
 	cout << ans << '\n';
 
