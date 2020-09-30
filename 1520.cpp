e4f65d8a260bf6515c87e001f7d4c00a8e0840fd
kyomin <kim031504@naver.com>
2020-10-01 01:15:07 +0900

    백준 1520 - 내리막 길 : DFS, 동적 계획법

--- 1520.cpp
+++ 1520.cpp
@@ -38,15 +38,16 @@ int dfs(int y, int x) {
 }
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	memset(dp, -1, sizeof(dp));
 
 	cin >> m >> n;
 
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < m; i++) 
+		for (int j = 0; j < n; j++) 
 			cin >> map[i][j];
-		}
-	}
 
 	cout << dfs(m - 1, n - 1) << '\n';
 
