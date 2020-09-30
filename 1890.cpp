a12997c8c5373824d79b44296619bf546b16607d
kyomin <kim031504@naver.com>
2020-10-01 02:07:13 +0900

    백준 1890 - 점프 : DFS, 동적 계획법

--- 1890.cpp
+++ 1890.cpp
@@ -10,9 +10,8 @@ int N;
 
 unsigned long long dfs(int y, int x) {
 	// 끝에 도달한 경우라면
-	if (y == N - 1 && x == N - 1) {
+	if (y == N - 1 && x == N - 1)
 		return 1;
-	}
 
 	// 아직 방문하지 않은 곳이라면
 	if (dp[y][x] == -1) {
@@ -22,28 +21,27 @@ unsigned long long dfs(int y, int x) {
 		int nextBottom = y + map[y][x];		// 아래로 가는 경우
 
 		// 지도 범위 내라면
-		if (nextRight < N) {
+		if (nextRight < N)
 			dp[y][x] += dfs(y, nextRight);
-		}
 
-		if (nextBottom < N) {
+		if (nextBottom < N)
 			dp[y][x] += dfs(nextBottom, x);
-		}
 	}
 
 	return dp[y][x];
 }
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	memset(dp, -1, sizeof(dp));
 
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
 			cin >> map[i][j];
-		}
-	}
 
 	cout << dfs(0, 0) << '\n';
 
