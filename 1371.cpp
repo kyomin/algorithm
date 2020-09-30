18767fe68f31e5ab85c0c297ab10480e099569d7
kyomin <kim031504@naver.com>
2020-10-01 00:48:44 +0900

    백준 1371 - 가장 많은 글자 : 문자열 처리

--- 1371.cpp
+++ 1371.cpp
@@ -16,20 +16,17 @@ int main() {
 	while (cin >> s) {	
 		int len = s.size();
 
-		for (int i = 0; i < len; i++) {
+		for (int i = 0; i < len; i++)
 			alphabet[(int)(s[i] - 'a')]++;
-		}
 
 		s.clear();
 	}
 
 	int max = *max_element(alphabet, alphabet + 26);
 
-	for (int i = 0; i < 26; i++) {
-		if (max == alphabet[i]) {
+	for (int i = 0; i < 26; i++)
+		if (max == alphabet[i])
 			cout << (char)(i + 'a');
-		}
-	}
 
 	cout << '\n';
 
