c369c713b7c158132c467918b2d97b52c398a3e1
kyomin <kim031504@naver.com>
2020-10-01 02:05:30 +0900

    백준 1834 - 나머지와 몫이 같은 수 : 수학, 구현

--- 1834.cpp
+++ 1834.cpp
@@ -5,14 +5,16 @@ using namespace std;
 typedef unsigned long long ll;
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int N;
 	cin >> N;
 
 	ll ans = 0;
 
-	for (int i = 1; i < N; i++) {
+	for (int i = 1; i < N; i++)
 		ans += (N*i) + i;
-	}
 
 	cout << ans << '\n';
 
