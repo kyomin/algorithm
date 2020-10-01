fbab9fc40cca70aa005d58d8c85e443aff13f5a2
kyomin <kim031504@naver.com>
2020-10-01 20:53:23 +0900

    백준 10101 - 삼각형 외우기 : 구현

--- 10101.cpp
+++ 10101.cpp
@@ -3,21 +3,20 @@
 using namespace std;
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int a, b, c;
 	cin >> a >> b >> c;
 
-	if (a == 60 && b == 60 && c == 60) {
+	if (a == 60 && b == 60 && c == 60)
 		cout << "Equilateral" << '\n';
-	}
-	else if ((a + b + c) == 180 && a == b || b == c || a == c) {
+	else if ((a + b + c) == 180 && a == b || b == c || a == c)
 		cout << "Isosceles" << '\n';
-	}
-	else if ((a + b + c) == 180 && a != b && b != c && a != c) {
+	else if ((a + b + c) == 180 && a != b && b != c && a != c)
 		cout << "Scalene" << '\n';
-	}
-	else if ((a + b + c) != 180) {
+	else if ((a + b + c) != 180)
 		cout << "Error" << '\n';
-	}
 
 	return 0;
 }
