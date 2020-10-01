d10ffe9d6963522ee4400e2a641d01fd11c9fcec
kyomin <kim031504@naver.com>
2020-10-01 21:24:39 +0900

    백준 10973 - 이전 순열 : 순열, 구현

--- 10973.cpp
+++ 10973.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int N;
 	vector<int> v;
 
@@ -12,20 +15,17 @@ int main() {
 
 	v.resize(N);
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
 		cin >> v[i];
-	}
 
 	if (prev_permutation(v.begin(), v.end())) {
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++)
 			cout << v[i] << ' ';
-		}
 
 		cout << '\n';
 	}
-	else {
+	else
 		cout << -1 << '\n';
-	}
 
 	return 0;
 }
