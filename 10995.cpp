3b029f92763f07b8c4c8d9c616159dce95720714
kyomin <kim031504@naver.com>
2020-10-01 21:27:52 +0900

    백준 10995 - 별 찍기20 : 구현

--- 10995.cpp
+++ 10995.cpp
@@ -4,22 +4,21 @@ using namespace std;
 
 void printStars(int N) {
 	for (int i = 1; i <= N; i++) {
-		if (i % 2 == 1) {
-			for (int j = 0; j < N; j++) {
+		if (i % 2 == 1)
+			for (int j = 0; j < N; j++)
 				cout << "* ";
-			}
-		}
-		else {
-			for (int j = 0; j < N; j++) {
+		else
+			for (int j = 0; j < N; j++)
 				cout << " *";
-			}
-		}
 
 		cout << endl;
 	}
 }
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int N; 
 	cin >> N;
 
