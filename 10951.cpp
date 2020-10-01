116e8aeb4dc7306ea2f29b0e3d4c9dd31ff26dcc
kyomin <kim031504@naver.com>
2020-10-01 21:21:50 +0900

    백준 10951 - A+B 4 : 수학, 구현

--- 10951.cpp
+++ 10951.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int a, b;
 
-	while (cin>>a>>b) {
+	while (cin>>a>>b)
 		cout << a + b << '\n';
-	}
 
 	return 0;
 }
