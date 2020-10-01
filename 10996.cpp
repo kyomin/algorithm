43c7737c3663632a666319a4f897ebb5c743ea3e
kyomin <kim031504@naver.com>
2020-10-01 21:29:16 +0900

    백준 10996 - 별 찍기21 : 구현

--- 10996.cpp
+++ 10996.cpp
@@ -6,26 +6,26 @@ void printStars(int N) {
 	int up = (N / 2) + (N % 2);
 	int down = (N / 2);
 
-	for (int i = 0; i < up; i++) {
+	for (int i = 0; i < up; i++)
 		cout << "* ";
-	}
 
-	cout << endl;
+	cout << '\n';
 
-	for (int i = 0; i < down; i++) {
+	for (int i = 0; i < down; i++)
 		cout << " *";
-	}
 
-	cout << endl;
+	cout << '\n';
 }
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int N;
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
 		printStars(N);
-	}
 
 	return 0;
 }
