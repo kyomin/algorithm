6e936f9f7a2223318002fdd1d58586d4cbe29195
kyomin <kim031504@naver.com>
2020-10-01 21:33:44 +0900

    백준 11024 - 더하기4 : 문자열 처리, 수학

--- 11024.cpp
+++ 11024.cpp
@@ -18,6 +18,9 @@ vector<string> split(string str, char delimiter) {
 }
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int T;
 	cin >> T;
 
@@ -34,9 +37,8 @@ int main() {
 
 		int len = nums.size();
 
-		for (int i = 0; i < len; i++) {
+		for (int i = 0; i < len; i++)
 			ans += atoi(nums[i].c_str());
-		}
 
 		cout << ans << '\n';
 	}
