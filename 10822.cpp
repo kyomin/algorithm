e717807e4571453a831d7d73b05622e2f931103c
kyomin <kim031504@naver.com>
2020-10-01 21:13:50 +0900

    백준 10822 - 더하기 : 문자열 처리, 수학

--- 10822.cpp
+++ 10822.cpp
@@ -18,6 +18,9 @@ vector<string> split(string str, char delimiter) {
 }
 
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	string str;
 	cin >> str;
 
@@ -27,9 +30,8 @@ int main() {
 
 	int len = nums.size();
 
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < len; i++)
 		ans += atoi(nums[i].c_str());
-	}
 
 	cout << ans << '\n';
 
