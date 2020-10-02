5fb1b7f18e3df55bb12f9f060f391d72ef308700
kyomin <kim031504@naver.com>
2020-10-02 20:47:44 +0900

    프로그래머스 - 주식가격 : 스택

---
+++ programmers_42584.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer;
+
+	int len = prices.size();
+
+	for (int i = 0; i < len; i++) {
+		int time = 0;
+
+		for (int j = i + 1; j < len; j++) {
+			time++;
+
+			if (prices[i] > prices[j])
+				break;
+		}
+
+		answer.push_back(time);
+	}
+
+	return answer;
+}
