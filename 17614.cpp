69430a5a1d840ca8c429b24ab14702dad850f07c
kyomin <kim031504@naver.com>
2020-10-02 19:48:31 +0900

    백준 17614 - 369 : 문자열 처리, 구현

--- 17614.cpp
+++ 17614.cpp
@@ -8,20 +8,21 @@ int ans = 0;
 void judge369(string str) {
 	int len = str.size();
 
-	for (int i = 0; i < len; i++) {
-		if (str[i] == '3' || str[i] == '6' || str[i] == '9') {
+	for (int i = 0; i < len; i++)
+		if (str[i] == '3' || str[i] == '6' || str[i] == '9')
 			ans++;
-		}
-	}
 }
 
 int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	int N;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++) {
+	for (int i = 1; i <= N; i++)
 		judge369(to_string(i));
-	}
 
 	cout << ans << '\n';
 
