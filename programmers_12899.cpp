8565c30c95723cfa59c8aad0b2283f09dc9df58a
kyomin <kim031504@naver.com>
2020-10-02 20:43:36 +0900

    프로그래머스 - 124 나라의 숫자 : 수학, 구현

--- programmers_12899.cpp
+++ programmers_12899.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 
 using namespace std;
@@ -26,16 +25,4 @@ string solution(int n) {
 	reverse(answer.begin(), answer.end());
 
 	return answer;
-}
-
-int main()
-{
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	for (int i = 1; i < 12; i++)
-		cout << solution(i) << '\n';
-
-	return 0;
 }
