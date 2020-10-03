643e7daad44d447f40778f12b640b77b931fd4fd
kyomin <kim031504@naver.com>
2020-10-03 18:46:17 +0900

    프로그래머스 - 나누어 떨어지는 숫자 배열 : 수학, 구현

---
+++ programmers_12910.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+	vector<int> answer;
+	int len = arr.size();
+
+	for (int i = 0; i < len; i++)
+		if (arr[i] % divisor == 0)
+			answer.push_back(arr[i]);
+
+	if (answer.size() == 0)
+		return { -1 };
+
+	sort(answer.begin(), answer.end());
+
+	return answer;
+}
