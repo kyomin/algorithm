8e28fc694026248ab1a68dac0c08a299705816b1
kyomin <kim031504@naver.com>
2020-10-04 23:19:49 +0900

    프로그래머스 - 문자열 다루기 기본 : 문자열 처리

---
+++ programmers_12918.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+	bool answer = true;
+	int len = s.size();
+
+	if (len != 4 && len != 6)
+		return false;
+
+	for (int i = 0; i < len; i++)
+		if (!isdigit(s[i]))
+			return false;
+
+	return answer;
+}
