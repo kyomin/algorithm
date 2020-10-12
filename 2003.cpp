590d8fd826d7312e3c1a8e6a9dd9730dacb4911e
kyomin <kim031504@naver.com>
2020-10-13 06:38:04 +0900

    백준 2003 - 수들의 합2 : 투 포인터

---
+++ 2003.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	int *A = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+		A[i] = n;
+	}
+
+	int s = 0, e = 0, count = 0, sum = 0;
+
+	while (true) {
+		if (sum == M)
+			count++;
+
+		if ((e == N) && (sum <= M))
+			break;
+
+		if (sum < M) {
+			sum += A[e];
+			e++;
+		}
+		else {
+			sum -= A[s];
+			s++;
+		}
+	}
+
+	cout << count << '\n';
+
+	return 0;
+}
