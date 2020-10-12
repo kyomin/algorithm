817e7d49cd33b7c505a4e2fd062525eef25f56a7
kyomin <kim031504@naver.com>
2020-10-13 04:00:17 +0900

    백준 1927 - 최소 힙 : 힙

---
+++ 1927.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+
+		if (n == 0) {
+			// 큐가 비어있다면!
+			if (pq.empty())
+				cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+			pq.push(n);
+	}
+
+	return 0;
+}
