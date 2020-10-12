450789fa2be580d3f4c49b286536f0256900c382
kyomin <kim031504@naver.com>
2020-10-13 02:56:13 +0900

    백준 1707 - 이분 그래프 : 그래프, DFS

---
+++ 1707.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> a[20001];
+int color[20001];
+
+void dfs(int node, int c) {
+	color[node] = c;
+
+	for (int i = 0; i < a[node].size(); i++) {
+		int next = a[node][i];
+
+		if (color[next] == 0)
+			dfs(next, 3 - c);
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		for (int i = 0; i <= 20000; i++)
+			color[i] = 0;
+
+		for (int i = 0; i <= 20000; i++)
+			a[i].clear();
+
+		int V, E;
+		cin >> V >> E;
+
+		for (int i = 0; i < E; i++) {
+			int u, v;
+			cin >> u >> v;
+
+			a[u].push_back(v);
+			a[v].push_back(u);
+		}
+
+		for (int i = 1; i <= V; i++)
+			if (color[i] == 0)
+				dfs(i, 1);
+
+		bool ok = true;
+
+		for (int i = 1; i <= V; i++) {
+			for (int k = 0; k < a[i].size(); k++) {
+				int j = a[i][k];
+
+				if (color[j] == color[i])
+					ok = false;
+			}
+		}
+
+		ok ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+
+	return 0;
+}
