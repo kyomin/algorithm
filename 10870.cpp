684ce18f593918abfac92a0ffdbbf4f70a707ff0
kyomin <kim031504@naver.com>
2020-10-14 07:29:13 +0900

    백준 10870 - 피보나치 수5 : 재귀

---
+++ 10870.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+	if (n == 0)
+		return 0;
+
+	if (n == 1)
+		return 1;
+
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	cout << fib(N) << endl;
+
+	return 0;
+}
