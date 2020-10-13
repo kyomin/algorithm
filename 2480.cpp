f98346c67147de32d40068e269e7229180d4c76c
kyomin <kim031504@naver.com>
2020-10-13 18:11:33 +0900

    백준 2480 - 주사위 세개 : 시뮬레이션

---
+++ 2480.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if ((a != b) && (b != c) && (a != c)) {
+		int big = max(max(a, b), c);
+		cout << big * 100 << endl;
+	}
+	else if ((a == b) && (b == c))
+		cout << 10000 + (a * 1000) << endl;
+	else {
+		if (a == b)
+			cout << 1000 + (a * 100) << endl;
+		else if (b == c)
+			cout << 1000 + (b * 100) << endl;
+		else
+			cout << 1000 + (a * 100) << endl;
+	}
+
+	return 0;
+}
