d5913b1c344404afc8a2a36a2659088a5b4320a2
kyomin <kim031504@naver.com>
2021-01-31 12:09:26 +0900

    SW Expert Academy 9317 - 석찬이의 받아쓰기 : 문자열

--- swea_9317.cpp
+++ swea_9317.cpp
@@ -16,10 +16,9 @@ int main() {
 
 		int ans = 0;
 
-		for (int i = 0; i < len; i++) {
+		for (int i = 0; i < len; i++)
 			if (str1[i] == str2[i])
 				ans++;
-		}
 
 		cout << '#' << t << ' ' << ans << '\n';
 	}
