04bbd6ed005c73893d13bccf2815273f335e143c
kyomin <kim031504@naver.com>
2021-02-12 13:06:29 +0900

    SW Expert Academy 2019 - 더블더블 : 구현

---
+++ swea_2019.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int num = 1;
+
+	for (int i = 0; i < N; i++) {
+		cout << num << ' ';
+		num *= 2;
+	}
+
+	cout << num << '\n';
+
+	return 0;
+}
