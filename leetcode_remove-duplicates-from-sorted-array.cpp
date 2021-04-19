dd61eb7d625080a16e81fae07e577094a4dbbe3d
kyomin <kim031504@naver.com>
2021-04-19 20:13:24 +0900

    릿코드 - Remove Duplicates from Sorted Array : 구현

---
+++ leetcode_remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+		return nums.size();
+	}
+};
