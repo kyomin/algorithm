ffd0392d5b1acec48cff739da57fe368139857f4
kyomin <kim031504@naver.com>
2021-04-19 21:17:04 +0900

    릿코드 - Search Insert Position : 구현

---
+++ leetcode_search-insert-position.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) {
+		int len = nums.size();
+
+		for (int i = 0; i < len; i++)
+			if (nums[i] == target || nums[i] > target)
+				return i;
+
+		return len;
+	}
+};
