947e849d8fa9e5a051c014561e3caa830223f6e4
kyomin <kim031504@naver.com>
2021-04-23 23:25:21 +0900

    릿코드 - Running Sum of 1d Array : 구현

---
+++ leetcode_running-sum-of-1d-array.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> runningSum(vector<int>& nums) {
+		vector<int> ans;
+		int sum = 0;
+
+		for (int num : nums) {
+			sum += num;
+			ans.push_back(sum);
+		}
+
+		return ans;
+	}
+};
