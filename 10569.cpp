450f67f6a2e289923ba13a08648a5a2a614c8260
kyomin <kim031504@naver.com>
2021-07-29 00:59:26 +0900

    백준 10569 - 다면체 : 수학

---
+++ 10569.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int t = 0; t < T; t++) {
+		int V, E;
+		cin >> V >> E;
+
+		cout << (2 + E - V) << '\n';
+	}
+
+	return 0;
+}
