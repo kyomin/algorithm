44ad7f2f6bdb60552dcf0e81c32df8dcf6d1c183
kyomin <kim031504@naver.com>
2021-08-04 00:31:02 +0900

    백준 7891 - Can you add this? : 수학

---
+++ 7891.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int t = 0; t < T; t++) {
+		int a, b;
+		cin >> a >> b;
+
+		cout << (a + b) << endl;
+	}
+
+	return 0;
+}
