adfdbd3bccc46070b5ce9837620c681eeec1b1c3
kyomin <kim031504@naver.com>
2021-09-26 06:01:01 +0900

    백준 4999 - 아! : 문자열

---
+++ 4999.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+
+	if (a.length() >= b.length())
+		cout << "go" << endl;
+	else
+		cout << "no" << endl;
+
+	return 0;
+}
