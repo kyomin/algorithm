c256a51e4a5e0d17d170a9675ff326985de8438c
kyomin <kim031504@naver.com>
2021-12-14 19:28:43 +0900

    백준 14645 - 와이버스 부릉부릉 : 구현

---
+++ 14645.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		int A, B;
+		cin >> A >> B;
+	}
+
+	cout << "ºñ¿ÍÀÌ" << endl;
+
+	return 0;
+}
