82f417472343cb5b9d0828399159186fd9ddc220
kyomin <kim031504@naver.com>
2022-03-08 03:41:27 +0900

    백준 9625 - BABBA : 다이나믹 프로그래밍

---
+++ 9625.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int DP[50][2];
+
+	cin >> n;
+
+	DP[0][0] = 0;
+	DP[0][1] = 0;
+	DP[1][0] = 0;
+	DP[1][1] = 1;
+	DP[2][0] = 1;
+	DP[2][1] = 1;
+
+	for (int i = 3; i <= n; i++) {
+		DP[i][0] = DP[i - 1][0] + DP[i - 2][0];
+		DP[i][1] = DP[i - 1][1] + DP[i - 2][1];
+	}
+
+	cout << DP[n][0] << " " << DP[n][1] << endl;
+
+	return 0;
+}
