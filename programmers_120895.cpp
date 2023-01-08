588ce0268311f01313c99aff1701db9e3a90df27
kyomin <kim031504@naver.com>
2023-01-09 05:12:28 +0900

    프로그래머스 - 인덱스 바꾸기 : 문자열 처리

---
+++ programmers_120895.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+	string answer = "";
+	int len = my_string.length();
+
+	for (int i = 0; i < len; i++) {
+		if (i == num1) {
+			answer += my_string[num2];
+			continue;
+		}
+
+		if (i == num2) {
+			answer += my_string[num1];
+			continue;
+		}
+
+		answer += my_string[i];
+	}
+
+	return answer;
+}
