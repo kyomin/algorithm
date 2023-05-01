91030bce74a3a904fbd8fbb0c7601f112f0b6fe6
kyomin <kim031504@naver.com>
2023-05-02 03:55:54 +0900

    백준 4779 - 칸토어 집합 : 구현, 재귀

---
+++ 4779.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void cantor(int n) {
+	int size = pow(3, n - 1);
+
+	// N = 0 인 경우 "-" 출력
+	if (n == 0) {
+		cout << "-";
+		return;
+	}
+
+	cantor(n - 1);
+	for (int i = 0; i < size; i++) {
+		cout << " ";
+	}
+	cantor(n - 1);
+}
+
+int main() {
+	int N;
+
+	while (cin >> N) {
+		cantor(N);
+		cout << "\n";
+	}
+
+	return 0;
+}
