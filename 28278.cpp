9a6ac754df9de1baf4582177490cc9b9fb55a631
kyomin <kim031504@naver.com>
2023-08-04 20:15:35 +0900

    백준 28278 - 스택 2 : 자료 구조, 스택

---
+++ 28278.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int N = 0;
+	scanf("%d", &N);
+	stack<int> st;
+
+	for (int i = 0; i < N; i++) {
+		int first = 0;
+		scanf("%d", &first);
+
+		if (first == 1) {
+			int second = 0;
+			scanf("%d", &second);
+			st.push(second);
+		}
+		else if (first == 2) {
+			if (st.empty()) {
+				printf("-1\n");
+			}
+			else {
+				printf("%d\n", st.top());
+				st.pop();
+			}
+		}
+		else if (first == 3) {
+			printf("%ld\n", st.size());
+		}
+		else if (first == 4) {
+			printf("%d\n", st.empty() ? 1 : 0);
+		}
+		else {
+			printf("%d\n", st.empty() ? -1 : st.top());
+		}
+	}
+
+	return 0;
+}
