0bd575a846b18674342cf51cb13806c573f0ea00
kyomin <kim031504@naver.com>
2023-08-09 03:38:59 +0900

    백준 1748 - 수 이어 쓰기 1 : 수학, 구현

--- 1748.cpp
+++ 1748.cpp
@@ -1,35 +1,20 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-// 정수의 자릿수를 출력하는 함수
-int Digit(int N) {
-	int count = 0;
-
-	if (N == 0)
-		return 1;
-
-	while (N != 0) {
-		N /= 10;
-		count++;
-	}
-
-	return count;
-}
-
-int main() {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	int count = 0;
+int main(void) {
 	int N;
+	int cnt = 0;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++)
-		count += Digit(i);
+	if (N < 10) {
+		cout << N;
+		return 0;
+	}
 
-	cout << count << endl;
+	for (int i = 1; i <= N; i *= 10)
+		cnt += N - i + 1;
+
+	cout << cnt;
 
 	return 0;
 }
